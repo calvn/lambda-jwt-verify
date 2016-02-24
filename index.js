72dcf379524003b9f2a17234bb699e63cbd2ee47
Calvin Leung Huang <cleung2010@gmail.com>
2016-02-24 13:05:37 -0500

    Parse Bearer from Authorization header

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ var jwt = require('jsonwebtoken');
 var secret = process.env.JWT_SECRET
 
 exports.handler = function(event, context) {
-  if(event.authorizationToken) {
+  if(event.authorizationToken && event.authorizationToken.split(' ')[0] === 'Bearer') {
     var secretBuf = new Buffer(secret, 'base64');
-    jwt.verify(event.authorizationToken, secretBuf, function(err, decoded) {
+    var idToken = event.authorizationToken.split(' ')[1];
+    jwt.verify(idToken, secretBuf, function(err, decoded) {
       if(err) {
-        console.log('failed jwt verify: ', err, 'auth: ', event.authorizationToken);
+        console.log('failed jwt verify: ', err, 'auth: ', idToken);
         return context.fail('authorization failure');
       } else {
         console.log('authorized:', decoded);
-        return context.succeed(decoded)
+        return context.succeed(decoded);
       }
     });
   } else {
-    console.log('invalid authorization token', event.authorizationToken);
+    console.log('invalid authorization token or header', event.authorizationToken);
     return context.fail('authorization failure');
   }
 };
