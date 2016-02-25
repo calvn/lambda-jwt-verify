7c340893448bd86a1cfcc21230357848435c4abe
Calvin Leung Huang <cleung2010@gmail.com>
2016-02-25 11:29:52 -0500

    Add exp verification

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ exports.handler = function(event, context) {
   if(event.authorizationToken && event.authorizationToken.split(' ')[0] === 'Bearer') {
     var secretBuf = new Buffer(secret, 'base64');
     var idToken = event.authorizationToken.split(' ')[1];
-    jwt.verify(idToken, secretBuf, function(err, decoded) {
-      if(err) {
+    var currentTime = Math.floor(Date.now() / 1000);
+    jwt.verify(idToken, secretBuf, function(err, payload) {
+      if (err) {
         console.log('failed jwt verify: ', err, 'auth: ', idToken);
         return context.fail('authorization failure');
+      } else if (payload.exp && payload.exp > currentTime) {
+        console.log('expired token, current time:', currentTime, 'exp: ', payload.exp);
+        return context.fail('authorization failure');
       } else {
-        console.log('authorized:', decoded);
-        return context.succeed(decoded);
+        console.log('authorized:', payload);
+        return context.succeed(payload);
       }
     });
   } else {
