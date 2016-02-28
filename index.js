a771ed6f3c3c7b5953407036d51ae7a626bfedfc
Calvin Leung Huang <cleung2010@gmail.com>
2016-02-28 02:21:25 -0500

    Create allow policy once authenticated

--- index.js
+++ index.js
@@ -14,10 +14,28 @@ exports.handler = function(event, context) {
         return context.fail('authorization failure');
       } else {
         console.log('authorized:', decoded);
+        return context.succeed(generatePolicy(decoded.sub, 'Allow', event.methodArn));
       }
     });
   } else {
     console.log('invalid authorization token or header', event.authorizationToken);
     return context.fail('authorization failure');
   }
+};
+
+var generatePolicy = function(principalId, effect, resource) {
+    var authResponse = {};
+    authResponse.principalId = principalId;
+    if (effect && resource) {
+        var policyDocument = {};
+        policyDocument.Version = '2012-10-17'; // default version
+        policyDocument.Statement = [];
+        var statementOne = {};
+        statementOne.Action = 'execute-api:Invoke'; // default action
+        statementOne.Effect = effect;
+        statementOne.Resource = resource;
+        policyDocument.Statement[0] = statementOne;
+        authResponse.policyDocument = policyDocument;
+    }
+    return authResponse;
 };
